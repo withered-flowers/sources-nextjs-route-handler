1d7a7a8ab100dbc3e61d1230c526c3fe5a8aaf04
Withered Flowers <withered-flowers@users.noreply.github.com>
2025-01-14 15:00:08 +0700

    feat: modify code for hash

--- sources/a-start/client-15/src/utils/hash.ts
+++
@@ -1,5 +0,0 @@
-import bcryptjs from "bcryptjs";
-
-export const hashText = (text: string): string => bcryptjs.hashSync(text);
-export const compareTextWithHash = (text: string, hash: string): boolean =>
-  bcryptjs.compareSync(text, hash);
--- sources/b-end/client-15/src/utils/hash.ts
+++ sources/b-end/client-15/src/utils/hash.ts
@@ -2,8 +2,15 @@ import { randomBytes, scryptSync, timingSafeEqual } from "node:crypto";
 
 const salt = randomBytes(16).toString("hex");
 
-export const generateHash = (plaintext: string) =>
-	scryptSync(plaintext, salt, 32).toString("hex");
+export const generateHash = (plaintext: string) => {
+	const key = scryptSync(plaintext, salt, 32);
 
-export const verifyHash = (plaintext: string, hash: string) =>
-	timingSafeEqual(scryptSync(plaintext, salt, 32), Buffer.from(hash, "hex"));
+	return `${salt}:${key.toString("hex")}`;
+};
+
+export const verifyHash = (plaintext: string, hash: string) => {
+	const [salt, key] = hash.split(":");
+	const keyBuffer = scryptSync(plaintext, salt, 32);
+
+	return timingSafeEqual(keyBuffer, Buffer.from(key, "hex"));
+};
